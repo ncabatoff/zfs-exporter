5cde3064eeeed4fb66ccfbb06930ce97d525c9a9
Nick Cabatoff <nick.cabatoff@gmail.com>
2016-07-23 18:34:22 -0400

    Collect ZFS metrics only when scraped (best practice for exporters)

--- zfs-exporter/main.go
+++ zfs-exporter/main.go
@@ -5,9 +5,8 @@ import (
 	"log"
 	"net/http"
 	_ "net/http/pprof"
-	"time"
 
-	"github.com/ncabatoff/go-libzfs"
+	"github.com/bicomsystems/go-libzfs"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
@@ -21,80 +20,67 @@ var (
 		"IoCtl",
 	}
 
-	vdevops = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Namespace: "zfs",
-		Subsystem: "zpool",
-		Name:      "vdevops_total",
-		Help:      "number of operations performed.",
-	}, []string{"poolname", "vdevtype", "vdevname", "vdevoptype"})
+	vdevopsDesc = prometheus.NewDesc(
+		"zfs_zpool_vdevops_total",
+		"number of operations performed.",
+		[]string{"poolname", "vdevtype", "vdevname", "vdevoptype"},
+		nil)
 
-	vdevbytes = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Namespace: "zfs",
-		Subsystem: "zpool",
-		Name:      "vdevbytes_total",
-		Help:      "number of bytes handled",
-	}, []string{"poolname", "vdevtype", "vdevname", "vdevoptype"})
+	vdevbytesDesc = prometheus.NewDesc(
+		"zfs_zpool_vdevbytes_total",
+		"number of bytes handled",
+		[]string{"poolname", "vdevtype", "vdevname", "vdevoptype"},
+		nil)
 
-	vdeverrors = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Namespace: "zfs",
-		Subsystem: "zpool",
-		Name:      "errors_total",
-		Help:      "number of errors seen",
-	}, []string{"poolname", "vdevtype", "vdevname", "errortype"})
+	vdeverrorsDesc = prometheus.NewDesc(
+		"zfs_zpool_errors_total",
+		"number of errors seen",
+		[]string{"poolname", "vdevtype", "vdevname", "errortype"},
+		nil)
 
-	vdevstate = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "zfs",
-		Subsystem: "zpool",
-		Name:      "vdevstate",
-		Help:      "vdev state: Unknown, Closed, Offline, Removed, CantOpen, Faulted, Degraded, Healthy.",
-	}, []string{"poolname", "vdevtype", "vdevname"})
+	vdevstateDesc = prometheus.NewDesc(
+		"zfs_zpool_vdevstate",
+		"vdev state: Unknown, Closed, Offline, Removed, CantOpen, Faulted, Degraded, Healthy.",
+		[]string{"poolname", "vdevtype", "vdevname"},
+		nil)
 
-	vdevalloc = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "zfs",
-		Subsystem: "zpool",
-		Name:      "allocated_bytes",
-		Help:      "number of bytes allocated (usage)",
-	}, []string{"poolname", "vdevtype", "vdevname"})
+	vdevallocDesc = prometheus.NewDesc(
+		"zfs_zpool_allocated_bytes",
+		"number of bytes allocated (usage)",
+		[]string{"poolname", "vdevtype", "vdevname"},
+		nil)
 
-	vdevspace = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "zfs",
-		Subsystem: "zpool",
-		Name:      "space_bytes",
-		Help:      "size of the vdev in bytes (total capacity).",
-	}, []string{"poolname", "vdevtype", "vdevname"})
+	vdevspaceDesc = prometheus.NewDesc(
+		"zfs_zpool_space_bytes",
+		"size of the vdev in bytes (total capacity).",
+		[]string{"poolname", "vdevtype", "vdevname"},
+		nil)
 
-	vdevfrag = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "zfs",
-		Subsystem: "zpool",
-		Name:      "fragmentation_percent",
-		Help:      "device fragmentation percentage",
-	}, []string{"poolname", "vdevtype", "vdevname"})
+	vdevfragDesc = prometheus.NewDesc(
+		"zfs_zpool_fragmentation_percent",
+		"device fragmentation percentage",
+		[]string{"poolname", "vdevtype", "vdevname"},
+		nil)
 
-	poolstate = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "zfs",
-		Subsystem: "zpool",
-		Name:      "poolstate",
-		Help:      "pool state enum: Active, Exported, Destroyed, Spare, L2cache, uninitialized, unavail, potentiallyactive",
-	}, []string{"poolname"})
+	poolstateDesc = prometheus.NewDesc(
+		"zfs_zpool_poolstate",
+		"pool state enum: Active, Exported, Destroyed, Spare, L2cache, uninitialized, unavail, potentiallyactive",
+		[]string{"poolname"},
+		nil)
 
-	poolstatus = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "zfs",
-		Subsystem: "zpool",
-		Name:      "poolstatus",
-		Help:      "pool status enum: CorruptCache, MissingDevR, MissingDevNr, CorruptLabelR, CorruptLabelNr, BadGUIDSum, CorruptPool, CorruptData, FailingDev, VersionNewer, HostidMismatch, IoFailureWait, IoFailureContinue, BadLog, Errata, UnsupFeatRead, UnsupFeatWrite, FaultedDevR, FaultedDevNr, VersionOlder, FeatDisabled, Resilvering, OfflineDev, RemovedDev, Ok",
-	}, []string{"poolname"})
+	poolstatusDesc = prometheus.NewDesc(
+		"zfs_zpool_poolstatus",
+		"pool status enum: CorruptCache, MissingDevR, MissingDevNr, CorruptLabelR, CorruptLabelNr, BadGUIDSum, CorruptPool, CorruptData, FailingDev, VersionNewer, HostidMismatch, IoFailureWait, IoFailureContinue, BadLog, Errata, UnsupFeatRead, UnsupFeatWrite, FaultedDevR, FaultedDevNr, VersionOlder, FeatDisabled, Resilvering, OfflineDev, RemovedDev, Ok",
+		[]string{"poolname"},
+		nil)
+)
+
+type (
+	ZfsCollector struct{}
 )
 
 func init() {
-	prometheus.MustRegister(vdevops)
-	prometheus.MustRegister(vdevbytes)
-	prometheus.MustRegister(vdeverrors)
-	prometheus.MustRegister(vdevstate)
-	prometheus.MustRegister(vdevalloc)
-	prometheus.MustRegister(vdevspace)
-	prometheus.MustRegister(vdevfrag)
-	prometheus.MustRegister(poolstate)
-	prometheus.MustRegister(poolstatus)
+	prometheus.MustRegister(&ZfsCollector{})
 }
 
 func main() {
@@ -103,8 +89,6 @@ func main() {
 		metricsPath   = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics.")
 	)
 	flag.Parse()
-
-	go collect()
 
 	http.Handle(*metricsPath, prometheus.Handler())
 
@@ -120,79 +104,105 @@ func main() {
 	http.ListenAndServe(*listenAddress, nil)
 }
 
-func collect() {
-	pools, err := zfs.PoolOpenAll()
-	if err != nil {
-		log.Fatal("error opening pools: %v", err)
-	}
-	for {
-		for _, pool := range pools {
-			poolstats(pool)
-		}
-		time.Sleep(1 * time.Second)
-	}
-	// TODO should we worry about shutting down gracefully and calling pool.Close()?
-}
-
 func poolname(pool zfs.Pool) string {
 	return pool.Properties[zfs.PoolPropName].Value
 }
 
-func poolstats(pool zfs.Pool) {
-	pool.RefreshStats()
-
-	poolName := poolname(pool)
-	pstatus, err := pool.Status()
-	if err != nil {
-		log.Printf("error getting status of pool '%s': %v\n", poolName, err)
-		pstatus = -1
-	}
-	poolstatus.WithLabelValues(poolName).Set(float64(pstatus))
-
-	pstate, err := pool.State()
-	pstateFloat := float64(pstate)
-	if err != nil {
-		log.Printf("error getting state of pool '%s': %v\n", poolName, err)
-		pstateFloat = -1
-	}
-	poolstate.WithLabelValues(poolName).Set(pstateFloat)
-
-	vdevStats(pool)
+// Describe implements prometheus.Collector.
+func (z *ZfsCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- vdevopsDesc
+	ch <- vdevbytesDesc
+	ch <- vdeverrorsDesc
+	ch <- vdevstateDesc
+	ch <- vdevallocDesc
+	ch <- vdevspaceDesc
+	ch <- vdevfragDesc
+	ch <- poolstateDesc
+	ch <- poolstatusDesc
+	// TODO add error metric
 }
 
-func vdevStats(pool zfs.Pool) {
-	vdt, err := pool.VDevTree()
+// Collect implements prometheus.Collector.
+func (z *ZfsCollector) Collect(ch chan<- prometheus.Metric) {
+	pools, err := zfs.PoolOpenAll()
 	if err != nil {
-		log.Printf("unable to read vdevtree for pool '%s': %v", poolname(pool), err)
+		log.Fatal("error opening pools: %v", err)
 	}
-	visitVdevs(pool, vdt, vdevCollector)
+
+	for _, pool := range pools {
+		ch <- prometheus.MustNewConstMetric(poolstateDesc,
+			prometheus.GaugeValue,
+			poolstate(pool),
+			poolname(pool))
+
+		ch <- prometheus.MustNewConstMetric(poolstatusDesc,
+			prometheus.GaugeValue,
+			poolstatus(pool),
+			poolname(pool))
+
+		vdt, err := pool.VDevTree()
+		if err != nil {
+			log.Printf("unable to read vdevtree for pool '%s': %v", poolname(pool), err)
+		}
+		visitVdevs(pool, vdt, func(pool zfs.Pool, vdt zfs.VDevTree) {
+			poolName := poolname(pool)
+			vType := string(vdt.Type)
+
+			ch <- prometheus.MustNewConstMetric(vdevstateDesc, prometheus.GaugeValue,
+				float64(vdt.Stat.State), poolName, vType, vdt.Name)
+			ch <- prometheus.MustNewConstMetric(vdevallocDesc, prometheus.GaugeValue,
+				float64(vdt.Stat.Alloc), poolName, vType, vdt.Name)
+			ch <- prometheus.MustNewConstMetric(vdevspaceDesc, prometheus.GaugeValue,
+				float64(vdt.Stat.Space), poolName, vType, vdt.Name)
+			ch <- prometheus.MustNewConstMetric(vdevfragDesc, prometheus.GaugeValue,
+				float64(vdt.Stat.Fragmentation), poolName, vType, vdt.Name)
+
+			ch <- prometheus.MustNewConstMetric(vdeverrorsDesc, prometheus.CounterValue,
+				float64(vdt.Stat.ReadErrors), poolName, vType, vdt.Name, "read")
+			ch <- prometheus.MustNewConstMetric(vdeverrorsDesc, prometheus.CounterValue,
+				float64(vdt.Stat.WriteErrors), poolName, vType, vdt.Name, "write")
+			ch <- prometheus.MustNewConstMetric(vdeverrorsDesc, prometheus.CounterValue,
+				float64(vdt.Stat.ChecksumErrors), poolName, vType, vdt.Name, "checksum")
+
+			for optype := zfs.ZIOTypeRead; optype < zfs.ZIOTypes; optype++ {
+				ch <- prometheus.MustNewConstMetric(vdevopsDesc, prometheus.CounterValue,
+					float64(vdt.Stat.Ops[optype]),
+					poolName, vType, vdt.Name, zioTypeNames[optype])
+			}
+
+			for optype := zfs.ZIOTypeRead; optype < zfs.ZIOTypes; optype++ {
+				ch <- prometheus.MustNewConstMetric(vdevbytesDesc, prometheus.CounterValue,
+					float64(vdt.Stat.Bytes[optype]),
+					poolName, vType, vdt.Name, zioTypeNames[optype])
+			}
+		})
+
+		pool.Close()
+	}
+
+}
+
+func poolstatus(pool zfs.Pool) float64 {
+	pstatus, err := pool.Status()
+	if err != nil {
+		log.Printf("error getting status of pool '%s': %v\n", poolname(pool), err)
+		return -1
+	}
+	return float64(pstatus)
+}
+
+func poolstate(pool zfs.Pool) float64 {
+	pstate, err := pool.State()
+	if err != nil {
+		log.Printf("error getting state of pool '%s': %v\n", poolname(pool), err)
+		return -1
+	}
+	return float64(pstate)
 }
 
 func visitVdevs(pool zfs.Pool, vdt zfs.VDevTree, visitor func(pool zfs.Pool, vdt zfs.VDevTree)) {
 	visitor(pool, vdt)
 	for _, child := range vdt.Devices {
 		visitVdevs(pool, child, visitor)
-	}
-}
-
-func vdevCollector(pool zfs.Pool, vdt zfs.VDevTree) {
-	poolName := poolname(pool)
-	vType := string(vdt.Type)
-
-	vdevstate.WithLabelValues(poolName, vType, vdt.Name).Set(float64(vdt.Stat.State))
-	vdevalloc.WithLabelValues(poolName, vType, vdt.Name).Set(float64(vdt.Stat.Alloc))
-	vdevspace.WithLabelValues(poolName, vType, vdt.Name).Set(float64(vdt.Stat.Space))
-	vdevfrag.WithLabelValues(poolName, vType, vdt.Name).Set(float64(vdt.Stat.Fragmentation))
-
-	vdeverrors.WithLabelValues(poolName, vType, vdt.Name, "read").Set(float64(vdt.Stat.ReadErrors))
-	vdeverrors.WithLabelValues(poolName, vType, vdt.Name, "write").Set(float64(vdt.Stat.WriteErrors))
-	vdeverrors.WithLabelValues(poolName, vType, vdt.Name, "checksum").Set(float64(vdt.Stat.ChecksumErrors))
-
-	for optype := zfs.ZIOTypeRead; optype < zfs.ZIOTypes; optype++ {
-		vdevops.WithLabelValues(poolName, vType, vdt.Name, zioTypeNames[optype]).Set(float64(vdt.Stat.Ops[optype]))
-	}
-
-	for optype := zfs.ZIOTypeRead; optype < zfs.ZIOTypes; optype++ {
-		vdevbytes.WithLabelValues(poolName, vType, vdt.Name, zioTypeNames[optype]).Set(float64(vdt.Stat.Bytes[optype]))
 	}
 }
